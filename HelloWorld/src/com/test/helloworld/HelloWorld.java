61e2e1094a800e79dc5a7283f2b2a6b9d62c4783
이명우 <dhfl1414@naver.com>
2017-01-13 12:51:05 +0900

    Helloworld 추가

---
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.test.helloworld;
+
+public class HelloWorld {
+	public static void main(String args[]){                //메인함수. ㄷㅊ고 외우면 됨, 이거부터 만들어라
+		System.out.println("Hello!! World!");              //콘솔에 "Hello! World!"라고 뜨게 함
+		System.out.println("ㅎㅇ요");
+	}
+}
